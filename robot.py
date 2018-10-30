7e8c29dcd3fec5ad4d8cac09795d03ecb365473d
Paige Vegna <paigevegna@gmail.com>
2018-10-30 16:20:23 -0700

    Fixed some syntax/imports
    imported DifferentialDrive from wpilib.drive and fixed some syntax errors

--- robot.py
+++ robot.py
@@ -1,7 +1,8 @@
 import wpilib as wpi
+from wpilib.drive import DifferentialDrive
 #from math import pi
 
-public class Robot(wpi.SampleRobot):
+class Robot(wpi.SampleRobot):
 
     def robotInit(self):
         #update channels
@@ -11,9 +12,9 @@ public class Robot(wpi.SampleRobot):
         self.rearL = wpi.Talon(3)
         self.gyro = wpi.AnalogGyro(0)
         self.joystick = wpi.Joystick(0)
-        self.right = wpi.SpeedControllerGroup(frontR, rearR)
-        self.left = wpi.SpeedControllerGroup(frontL, rearL)
-        self.dTrain = wpi.DifferentialDrive(self.right, self.left)
+        self.right = wpi.SpeedControllerGroup(self.frontR, self.rearR)
+        self.left = wpi.SpeedControllerGroup(self.frontL, self.rearL)
+        self.dTrain = wpi.drive.DifferentialDrive(self.right, self.left)
 
     def autonomous(self):
         while self.isAutonomous() and self.isEnabled():
@@ -21,7 +22,7 @@ public class Robot(wpi.SampleRobot):
 
     def disabled(self):
         while self.isDisabled():
-            self.dtrain.arcadeDrive(0.0, 0.0)
+            self.dTrain.arcadeDrive(0.0, 0.0)
 
     def operatorControl(self):
         while self.isOperatorControl() and self.isEnabled():
